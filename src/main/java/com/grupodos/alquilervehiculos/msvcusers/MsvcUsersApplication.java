c6c1b1d269b8a481f8d7e130073e601087e19462
Alexander <u23241864@utp.edu.pe>
2025-12-02 08:27:48 -0500

    se migro a un repositorio independiente

---
+++ src/main/java/com/grupodos/alquilervehiculos/msvcusers/MsvcUsersApplication.java
@@ -0,0 +1,21 @@
+package com.grupodos.alquilervehiculos.msvcusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
+public class MsvcUsersApplication {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcUsersApplication.class, args);
+    }
+
+}
